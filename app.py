95e7e1a1cffcd2e39b1396686ce67ae3d6b2f51f
Alan Wood <WoodAlanM@gmail.com>
2020-04-05 21:13:02 -0400

    Added the ability to add records to the database

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 
 from peewee import *
 
@@ -20,7 +20,7 @@ class Entry(Model):
 
 
 def initialize():
-    db.connect()
+    db.connect(reuse_if_open=True)
     db.create_tables([Entry], safe=True)
 
 
@@ -36,20 +36,34 @@ def add_data(title, date, time_spent, what_learned, resources):
     except IntegrityError:
         return False
 
+def update_data()
 
-def get_data():
+
+def get_data(id=None):
     list_of_entries = Entry.select().order_by(Entry.entry_id)
-    return_list = []
-    for entry in list_of_entries:
-        temp_list = entry.resources.split(", ")
-        entry_list = [entry.entry_id,
-                      entry.entry_title,
-                      entry.entry_date,
-                      entry.time_spent,
-                      entry.what_learned,
-                      temp_list]
-        return_list.append(entry_list)
-    return return_list
+    if id:
+        list_of_entries = list_of_entries.where(Entry.entry_id == id)
+        an_entry = []
+        for entry in list_of_entries:
+            an_entry.append(entry.entry_id)
+            an_entry.append(entry.entry_title)
+            an_entry.append(entry.entry_date)
+            an_entry.append(entry.time_spent)
+            an_entry.append(entry.what_learned)
+            an_entry.append(entry.resources)
+        return an_entry
+    else:
+        return_list = []
+        for entry in list_of_entries:
+            temp_list = entry.resources.split(", ")
+            entry_list = [entry.entry_id,
+                          entry.entry_title,
+                          entry.entry_date,
+                          entry.time_spent,
+                          entry.what_learned,
+                          temp_list]
+            return_list.append(entry_list)
+        return return_list
 
 
 @app.route("/")
@@ -64,12 +78,48 @@ def index():
     return render_template("index.html", titledates=list_of_items)
 
 
-@app.route("/entries/<id>")
+@app.route("/entries/<int:id>")
 def view_details(id):
     initialize()
     list_of_entries = get_data()
-    detail_data = list_of_entries[int(id) - 1]
+    detail_data = list_of_entries[id- 1]
     return render_template("detail.html", details=detail_data)
+
+
+@app.route("/entries/new", methods=["GET", "POST"])
+def add_new():
+    initialize()
+    if request.method == "GET":
+        return render_template("new.html")
+    elif request.method == "POST":
+        data = dict(request.form.items())
+        add_data(data["title"],
+                 data["date"],
+                 data['timeSpent'],
+                 data["whatILearned"],
+                 data["ResourcesToRemember"]
+                 )
+        return index()
+
+
+@app.route("/entries/<int:id>/edit", methods=["GET", "POST"])
+def edit(id):
+    initialize()
+    if request.method == "GET":
+        an_entry = get_data(id)
+        resources_string = ", ".join(an_entry[5])
+        new_entry = an_entry[0:5]
+        new_entry.append(resources_string)
+        return render_template("edit.html", details=an_entry)
+    elif request.method == "POST":
+        data = dict(request.form.items())
+        add_data(data["title"],
+                 data["date"],
+                 data['timeSpent'],
+                 data["whatILearned"],
+                 data["ResourcesToRemember"]
+                 )
+        return index()
 
 
 app.run(debug=True, host='127.0.0.1', port=8000)
